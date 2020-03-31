9c7a24b1a18ac2103655f4d612b918d5fca983da
James <jamesrp13@gmail.com>
2020-03-30 21:07:48 -0600

    Create initial project

---
+++ Catfish/Catfish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Catfish
+//
+//  Created by James Pacheco on 3/30/20.
+//  Copyright © 2020 James Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
