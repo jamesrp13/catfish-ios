96972ae99a43a3803d7722776191a15bd48f68fa
Dan Morse <djmthedev@gmail.com>
2020-03-31 10:03:44 -0700

    added UserController

---
+++ Catfish/Catfish/Model Controllers/UserController.swift
@@ -0,0 +1,13 @@
+//
+//  UserController.swift
+//  Catfish
+//
+//  Created by morse on 3/31/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    var user: User?
+}
