f4cc94b876b7b61448202b17f22224cab74fab74
brandi <blubrandi0@gmail.com>
2020-04-01 11:04:05 -0600

    AddGameViewController and CFGameCell
    Created custom Game UICollectionViewCell class and added AddGameViewController class

---
+++ Catfish/Catfish/Custom Views/CFGameCell.swift
@@ -0,0 +1,13 @@
+//
+//  CFGameCell.swift
+//  Catfish
+//
+//  Created by Brandi Bailey on 4/1/20.
+//  Copyright © 2020 James Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class CFGameCell: UICollectionViewCell {
+    
+}
---
+++ Catfish/Catfish/ViewControllers/AddGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddGameViewController.swift
+//  Catfish
+//
+//  Created by Brandi Bailey on 4/1/20.
+//  Copyright © 2020 James Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class AddGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
