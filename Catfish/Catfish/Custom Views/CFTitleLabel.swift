d0c8efb2961107aff4edb6d0885cf4612460e1dd
Dan Morse <djmthedev@gmail.com>
2020-03-31 17:39:25 -0700

    added CFTitleLabel

---
+++ Catfish/Catfish/Custom Views/CFTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  CFTitleLabel.swift
+//  Catfish
+//
+//  Created by morse on 3/31/20.
+//  Copyright © 2020 James Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class CFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(textAlignment: NSTextAlignment = .center, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.8
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
