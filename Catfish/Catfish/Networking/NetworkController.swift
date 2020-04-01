fa177be23b7be25663335000e7a48524960a8f82
Dan Morse <djmthedev@gmail.com>
2020-03-31 17:41:23 -0700

    added NetworkController

---
+++ Catfish/Catfish/Networking/NetworkController.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkController.swift
+//  Catfish
+//
+//  Created by morse on 3/31/20.
+//  Copyright © 2020 James Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class NetworkController {
+    let cache = NSCache<NSString, UIImage>()
+}
