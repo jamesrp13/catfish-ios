c174aa9ec89467c0457b2e5cff8524ec0a7eaf46
Shawn Gee <shawnge@gmail.com>
2020-04-02 08:19:16 -0400

    Added CircularImageView for convenience

---
+++ Catfish/Catfish/Custom Views/CircularImageView.swift
@@ -0,0 +1,32 @@
+//
+//  CircularImageView.swift
+//  Catfish
+//
+//  Created by Shawn Gee on 4/1/20.
+//  Copyright © 2020 James Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class CircularImageView: UIImageView {
+    
+    public init(width: CGFloat, image: UIImage? = nil) {
+        super.init(image: image)
+        contentMode = .scaleAspectFill
+        translatesAutoresizingMaskIntoConstraints = false
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        heightAnchor.constraint(equalTo: widthAnchor).isActive = true
+        clipsToBounds = true
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.width / 2
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+}
