93e8b6a39c76256b9a8da46abdf750be0ef18dfa
Shawn Gee <shawngee@gmail.com>
2020-04-04 08:16:48 -0400

    Added colors to asset folder and constants, made BGGradientView

--- Catfish/Catfish/Utilities/Constants.swift
+++ Catfish/Catfish/Utilities/Constants.swift
@@ -14,7 +14,15 @@ enum Images {
 }
 
 enum Colors {
-    static let purple = UIColor(named: "Purple")!
+    static let purple = UIColor(named: "BGGradientTop")! // TODO: Delete after switching colors to final scheme
+    
+    static let aqua = UIColor(named: "Aqua")!
+    static let bgGradientBottom = UIColor(named: "BGGradientBottom")!
+    static let bgGradientTop = UIColor(named: "BGGradientTop")!
+    static let blue = UIColor(named: "Blue")!
+    static let gray = UIColor(named: "Gray")!
+    static let pink = UIColor(named: "Pink")!
+    static let white = UIColor(named: "White")! // Use this in case we want to have a custom color for dark mode
 }
 
 enum Fonts {
